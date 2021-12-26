a12ea8ae6413aba6c2fc4b8f36764017b828942d
Vladimir <68743149+vpenywise@users.noreply.github.com>
2021-12-26 23:10:21 +0200

    Update Testing.java

--- Testing.java
+++ Testing.java
@@ -1,33 +1,40 @@
 package testing;
 
 import static org.junit.Assert.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.Before;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import CoolTrick.Sashko;
 
 class Testing {
+	
+	Sashko testSashko = new Sashko();
+	List<Integer> testNumber = new ArrayList<>();
 
 	@Test
 	void testInputWithinRange() {
 
-		Sashko testSashko = new Sashko();
-		List<Integer> testNumber = new ArrayList<>();
+		
+		testSashko.checkUserInput(9, 3, testNumber);
 
 		assertTrue(testSashko.askUserInput() == 3);
 
-		testSashko.checkUserInput(9, 3, testNumber);
-
+	}
+	
+	@Test
+	public void testSize() {
+//		assertEquals("Testing the size of the List", 3, testNumber);
+	}
+	
 //		testSashko.checkUserInput(9, 3, firstNumber);
 //		askUserInput(testSashko);
 //		checkCorrectUserInput(testSashko);
 //		checkWrongtUserInput(testSashko);
-
-	}
 
 //	}
 //
