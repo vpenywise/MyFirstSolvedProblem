4f0d79c729f486d25e0ca584e92ba8b0189f9ffb
Vladimir <68743149+vpenywise@users.noreply.github.com>
2021-12-28 20:50:26 +0200

    Update SashkoTest.java

--- src/test/java/cool/trick/SashkoTest.java
+++ src/test/java/cool/trick/SashkoTest.java
@@ -1,35 +1,43 @@
-package cool.trick;
+package main.pack;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.List;
+public class MyOtherClassTest {
+	
+	@Test
+	public void testMyString() {
+		MyOtherClass myOtherClassTest = new MyOtherClass();
+		testMyString(myOtherClassTest, "WLA DO");
+	}
+	
+	@Test
+	public void testMySumTwoDigitsMethod() {
+		MyOtherClass myOtherClassTest = new MyOtherClass();
+		testMySumTwoDigitsMethod(myOtherClassTest);
+	}
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
-class SashkoTest {
-
-    @Test
-    public void test() {
-        Sashko sashko = new Sashko();
-        List<Integer> testNumber = new ArrayList<>();
-        sashko.checkUserInput(9, 3, testNumber);
-    }
-
-    @Test
-    public void checkWrongtUserInput() {
-        Sashko sashko = new Sashko();
-        List<Integer> firstNumber = new ArrayList<>();
-        Integer integer = sashko.checkUserInput(9, 10, firstNumber);
-        //some assert
-    }
-
-    @Test
-    public void checkCorrectUserInput() {
-        Sashko sashko = new Sashko();
-        List<Integer> firstNumber = new ArrayList<>();
-        Integer integer = sashko.checkUserInput(9, 3, firstNumber);
-        //some assert
-    }
+	@Test
+	public void testMySumTwoDigitsMethod_WrongResult() {
+		MyOtherClass myOtherClassTest = new MyOtherClass();
+		testMySumTwoDigitsMethod_WrongResult(myOtherClassTest);
+	}
+	
+	private void testMyString(MyOtherClass myOtherClassTest, String expectedResult) {
+		expectedResult = "";
+		String returnResult = myOtherClassTest.myTestString(expectedResult);
+		assertEquals(expectedResult, returnResult);
+	}
+	
+	private void testMySumTwoDigitsMethod(MyOtherClass myOtherClassTest) {
+		int actualResult = myOtherClassTest.sumTwoDigits(2, 3);
+		assertEquals(5, actualResult);
+	}
+	
+	private void testMySumTwoDigitsMethod_WrongResult(MyOtherClass myOtherClassTest) {
+		int actualWrongResult = myOtherClassTest.sumTwoDigits(2, 3);
+		assertNotEquals(7, actualWrongResult);
+	}
 }
